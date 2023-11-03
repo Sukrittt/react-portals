520646240ace5f87c6b8adf8553a60a0b828cb61
Sukrit Saha <sukritsaha27@gmail.com>
2023-11-04 00:03:03 +0530

    empty-chat display

--- src/components/dashboard-cards/chats.tsx
+++ src/components/dashboard-cards/chats.tsx
@@ -1,5 +1,13 @@
+import { CardContent } from "../ui/card";
+
 const Chats = () => {
-  return <div>Card Content</div>;
+  return (
+    <CardContent className="py-3 h-full flex items-center justify-center">
+      <p className="text-muted-foreground font-medium text-sm">
+        No chats to display
+      </p>
+    </CardContent>
+  );
 };
 
 export default Chats;
